fce73feb270ea70313ca1fba678c1581e847b1a7
Daniele de Moraes <danimoraes.barbosa@gmail.com>
2021-03-05 22:41:26 -0300

    Include simple tests for googleSearch functionality

--- script.test.js
+++ script.test.js
@@ -7,8 +7,28 @@ const dbMock = [
     'happydog.com',
     'macaroni.com',
     'sopranos.com',
+    'dogsareawesome.com',
+    'dogsdogsdogs.com'
 ];
 
-it('test', () => {
-    googleSearch('testetsts', dbMock)
+describe('googleSearch', () => {
+    it('is searching invalid word', () => {
+        expect(googleSearch('invalid', dbMock)).toEqual([])
+    });
+
+    it('is searching valid word with 2 results', () => {
+        expect(googleSearch('dogs', dbMock)).toEqual(['dogsareawesome.com',
+            'dogsdogsdogs.com'])
+    });
+
+    it('is searching valid word with top 3 results', () => {
+        expect(googleSearch('dog', dbMock).length).toEqual(3);
+    });
+
+    it('works with undefined and null', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    });
 })
+
+
